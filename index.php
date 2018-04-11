1550bb1f1efa722cbcf06e06fec815d18ad03754
daw2 <daw2@daw2m08.fjeclot.net>
2018-04-11 20:53:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html> 
+    <head> 
+        <meta charset="UTF­8"> 
+        <title>Desplegant una aplicaci
+ó
+ PHP sobre Heroku</title> 
+    </head> 
+    <body> 
+        <?php 
+            echo "Afegint Cookies<br>"; 
+            echo "Sóc el desenvolupador xxxx"; 
+            setcookie("nom", "Jordi Pons", time()+3600, "/", "", 0); 
+            setcookie("edat", "21", time()+3600, "/", "", 0); 
+            setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
+            setcookie("curs", "2n", time()+3600, "/", "", 0); 
+            setcookie("any", "2017­2018", time()+3600, "/", "", 0);            
+        ?>  
+    </body>    
+</html> 
